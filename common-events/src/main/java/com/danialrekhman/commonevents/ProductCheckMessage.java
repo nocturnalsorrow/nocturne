61de6f62bc33315ad1321e03efe32c8dc908dff8
Danial Rehman <danial.rehman36@gmail.com>
2025-07-17 19:38:06 +0300

    Set up `common-events` module with Maven Wrapper, configurations, and Docker Compose for Kafka
    Initialized the `common-events` module with a Maven Wrapper and basic configurations, including `.gitattributes`, `.gitignore`, and `application.properties`. Added IntelliJ project files and Docker Compose setup for Kafka/Zookeeper services to facilitate local development.

---
+++ common-events/src/main/java/com/danialrekhman/commonevents/ProductCheckMessage.java
@@ -0,0 +1,15 @@
+package com.danialrekhman.commonevents;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class ProductCheckMessage {
+    private String correlationId;
+    private Long productId;
+    private int quantity;
+    private BigDecimal priceAtOrder;
+    private Boolean available;
+    private String message;
+}
